a6044f543a62ab84f54bf05e7627de5110fedd42
DR-INFINITY-92 <104050350+DR-INFINITY-92@users.noreply.github.com>
2022-04-22 07:03:46 +0200

    Create 1.4.1.c

---
+++ Funkcije/1.4.1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+int minimum(int x,int y,int z)
+{
+    if(x<=y&&x<=z)
+        return x;
+    else
+        if(y<=x&&y<=z)
+            return y;
+        else
+        if(z<=x&&z<=y)
+            return z;
+}
+
+int main()
+{
+    int a,b,c,m;
+    printf("unesite brojeve a,b,c\n");
+    scanf("%d%d%d",&a,&b,&c);
+    m=minimum(a,b,c);
+    printf("najmanji broj od unetih brojeva %d %d %d je %d",a,b,c,m);
+    return 0;
+}
