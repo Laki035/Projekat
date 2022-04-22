1676fb94a569b537c2c062c9f55482d681ab72cc
DR-INFINITY-92 <104050350+DR-INFINITY-92@users.noreply.github.com>
2022-04-22 07:10:37 +0200

    Update 1.4.1-V2.c

--- Funkcije/1.4.1-V2.c
+++ Funkcije/1.4.1-V2.c
@@ -9,8 +9,8 @@ int minimum(int x,int y,int z)
         if((y<=x)&&(y<=z))
             return y;
         else
-   if((z<=x)&&(z<=y))
-            return z;
+            if((z<=x)&&(z<=y))
+                return z;
 }
 
 int main()
