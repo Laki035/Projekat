2348b8396b94e2d9ba9d93eb2088570826815c2a
DR-INFINITY-92 <104050350+DR-INFINITY-92@users.noreply.github.com>
2022-04-23 10:35:52 +0200

    Create 1.4.17-V1.c

---
+++ Funkcije/1.4.17-V1.c
@@ -0,0 +1,32 @@
+/*basic verzija ispitivanje da li su cifre u unetom broju par nepar orijentisane*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int par_nepar(int x)
+{
+	int indikator=0,pc;
+	x=abs(x);
+    pc=x%10;
+    x=x/10;
+	while(x>0)
+	{
+		if(((x%10)%2)==(pc%2))
+		{
+			indikator=1;
+            break;
+		}
+        pc=x%10;
+		x=x/10;
+	}
+	return indikator;
+}
+void main() 
+{
+	int n,c;
+		printf("unesi broj n\n");
+		scanf("%d",&n);
+            if(par_nepar(n)==0)
+                printf("broj %d, ispunjava uslov i sastoji se iz naizmenicno parno neparnih cifara\n",n);
+            else
+                printf("broj %d, ne ispunajava uslov i ne stoji iz naizmenicno parno neparnih cifara\n",n);	
+}
