628cd6aee9a0e77679e1b0a2397ec2c942d9f913
DR-INFINITY-92 <104050350+DR-INFINITY-92@users.noreply.github.com>
2022-04-29 12:49:16 +0200

    Create 1.c

---
+++ Nizovi 1D/1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main()
+{
+    int n,i;
+    printf("unesi koliko elemnata niza ima\n");
+    scanf("%d", &n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+       printf("unesi %d. element niza\n",i+1);
+       scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+       printf("a[%d] = %d",i,a[i]);
+       printf("\n");
+    }
+}
