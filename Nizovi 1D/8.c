91fc20b612fc2f68d945e114c5b9bd00618ae356
DR-INFINITY-92 <104050350+DR-INFINITY-92@users.noreply.github.com>
2022-05-03 20:04:36 +0200

    Delete 8.c

--- Nizovi 1D/8.c
+++ Nizovi 1D/8.c
@@ -1,4 +1,23 @@
-dfdfdfdfdfdfdf
-printf("");
-int i = 0;
-printf("unesi koliko");
+#include<stdio.h>
+#include<stdlib.h>
+unos_niza(int a[],int n)
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+         printf("unesi %d. element niza\n",i+1);
+         scanf("%d",&a[i]);
+    }
+}
+ispis_niza(int a[],int n)
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+         printf("a[%d]=%d\n",i,a[i]);
+    }
+}
+void main()
+{
+
+}
