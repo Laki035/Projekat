0381e9d9cb7e84afd4fe2faff0bfcf08eeb6a3db
DR_INFINITY_92 <d.rajkovicrs.o@gmail.com>
2022-05-10 08:19:45 +0200

    asa

---
+++ Nizovi 1D/19 ispis u main funkcija vraca proizvod.c
@@ -0,0 +1,42 @@
+ #include<stdio.h>
+#include<stdlib.h>
+void unos_niza(int a[],int n)
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+         printf("unesi %d. element niza\n",i+1);
+         scanf("%d",&a[i]);
+    }
+}
+void ispis_niza(int a[],int n)
+{
+    int i;
+    for(i=0;i<n;i++)
+    {
+         printf("a[%d]=%d",i,a[i]);
+    }
+}
+int proizvod_clanova_niza_sa_pi(int a[],int n)
+{
+    int i,p=1;
+    for(i=0;i<n;i++)
+    {
+        if(i%2==0)
+            p*=a[i]; /*skraceni zapis od p=p*a[i];*/
+    }
+    return p;
+   
+}
+void main()
+{
+    int n;
+    printf("unesi koliko ima elemenata niza\n");
+    scanf("%d",&n);
+    int a[n];
+    unos_niza(a,n);
+    ispis_niza(a,n);
+    printf("proizvod elemenata niza sa parnim indeksom je %d",proizvod_clanova_niza_sa_pi(a,n););
+  
+}
+
